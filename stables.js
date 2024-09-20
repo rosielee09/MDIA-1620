05ab3d18dc17a78854ec523e209b3084eace2214
Rosie Lee <blee351@my.bcit.ca>
2024-09-20 15:43:14 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,8 @@
+let horseName = "Stephanie";
+console.log(horseName);
+horseName = "Charlie";
+console.log(horseName);
+let horseAge = 12346;
+console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
