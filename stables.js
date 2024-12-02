5bc8366d37786adf10c04dff26af6219af0ff746
Rosie Lee <blee351@my.bcit.ca>
2024-12-01 20:26:27 -0800

    Add Horse constuctor, object literal and store them in horses variable

--- stables.js
+++ stables.js
@@ -17,3 +17,51 @@ let horses=[];
 let visitorMessage =`Welcome to our Stable! We hope you enjoy your visit.` 
 const LATE_PAYMENT_FEE = 50;
 let availableStalls = 10;
+
+
+//------------------------- First day -------------------------//
+function Horse(name, nickname, favTreat, age, monthlyRent, isInside, hobby, favColor){
+    this.name = name;
+    this.nickname = nickname;
+    this.favTreat = favTreat;
+    this.age = age;
+    this.monthlyRent = monthlyRent;
+    this.isInside = isInside;
+    this.hobby = hobby;
+    this.favColor = favColor;
+
+    this.intro = function() {
+        return `Hi! Let me introduce my horse ${this.name} who is ${this.age} years old, and loves  ${this.favTreat}.`;
+    }
+
+    this.getFavColor = function(){
+        return `${this.name}'s favorite color is ${this.favColor}`;
+    }
+}
+
+const horse1 = new Horse("Jenny", "Jen", "carrots", 3, 300, true, "running", 'pink');
+const horse2 = new Horse("Rose", "Rosie", "apples", 5, 280, false, "drawing", "blue" );
+const horse3 = new Horse("Lisa", "Li", "radish", 7, 250, true, "walking", "green");
+
+horses.push(horse1, horse2, horse3);
+
+const newHorse = {
+    name: "Jisu",
+    nickname: "Blink",
+    favTreat: "cucumber",
+    age: 6,
+    monthlyRent: 150,
+    isInside: false,
+    hobby: "singing",
+    favColor:"lavender",
+
+    intro: function() {
+        return `Hi! Let me introduce my horse ${this.name}. ${this.name} is ${this.age} years old, and ${this.name}'s favorite snack is ${this.favTreat}.`;
+    },
+
+    getFavColor: function(){
+        return `${this.name}'s favorite color is ${this.favColor}`;
+    },
+
+}
+horses.push(newHorse);
